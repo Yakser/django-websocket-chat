3805ee99113d677dc138d6f7cb7701e1cb6c45c3
Sergey Yaksanov <iaksanov2044@mail.ru>
2022-05-01 19:43:18 +0400

    Отображение сайдбара в тестовой группе

--- chat/groups/urls.py
+++ chat/groups/urls.py
@@ -4,5 +4,5 @@ from groups.views import GroupsView
 
 app_name = 'groups'
 urlpatterns = [
-    path('<slug:group_name>/', GroupsView.as_view(), name='groups'),
+    path('<slug:group_name>/', GroupsView.as_view(), name='group'),
 ]
