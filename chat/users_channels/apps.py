016163d67692cff018664719a945bda6c859c95f
Sergey Yaksanov <iaksanov2044@mail.ru>
2022-05-03 13:26:07 +0400

    Добавил поле slug  и переопределил поля для каналов

--- chat/users_channels/apps.py
+++ chat/users_channels/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class UsersChannelsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users_channels'
+    verbose_name = 'Каналы'
--- chat/users_channels/models.py
+++ chat/users_channels/models.py
@@ -1,17 +1,18 @@
-from django.db import models
-from sorl.thumbnail import get_thumbnail
-
-from django.utils.html import mark_safe
-
 from core.models import BaseWebsocketGroup
+from django.db import models
 
 
 class UsersChannel(BaseWebsocketGroup):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        image = self._meta.get_field('image')
-        image.verbose_name = 'Изображение канала'
-        image.help_text = 'Выберите изображение канала'
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     image = self._meta.get_field('image')
+    #     image.verbose_name = 'Изображение канала'
+    #     image.help_text = 'Выберите изображение канала'
+
+    image = models.ImageField(verbose_name='Изображение канала',
+                              upload_to='uploads/groups_images',
+                              null=True,
+                              help_text='Выберите изображение канала')
 
     class Meta:
         verbose_name = 'Канал'
