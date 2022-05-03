d1ffcd7f12fed4f25e7dc41ec7ef923a2ef5525c
Sergey Yaksanov <iaksanov2044@mail.ru>
2022-05-03 14:50:23 +0400

    дропнул миграции, пофиксил создание моделей групп и каналов

--- chat/users_channels/models.py
+++ chat/users_channels/models.py
@@ -1,5 +1,8 @@
 from core.models import BaseWebsocketGroup
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class UsersChannel(BaseWebsocketGroup):
@@ -7,6 +10,16 @@ class UsersChannel(BaseWebsocketGroup):
                               upload_to='uploads/groups_images',
                               null=True,
                               help_text='Выберите изображение канала')
+    owner = models.ForeignKey(User,
+                              verbose_name='Владелец',
+                              related_name='owner_users_channels',
+                              on_delete=models.SET_NULL,
+                              null=True)
+
+    group_members = models.ManyToManyField(User,
+                                           verbose_name='Участники',
+                                           related_name='users_channels'
+                                           )
 
     class Meta:
         verbose_name = 'Канал'
