c41062e83013037183a920ceeb52a84c3f585352
Sergey Yaksanov <iaksanov2044@mail.ru>
2022-05-08 13:50:34 +0400

    Пофиксил меню и увеличение textarea

--- chat/static_dev/js/auto_textarea_resizing.js
+++ chat/static_dev/js/auto_textarea_resizing.js
@@ -1,5 +1,9 @@
-function autoheight(element) {
-    let el = document.getElementById(element);
-        el.style.height = "5px";
-        el.style.height = (el.scrollHeight)+"px";
+const autoTextareaResizing = () => {
+    const textarea = document.getElementById('chat-message-input');
+    textarea.addEventListener('input', (event) => {
+        textarea.style.height = "5px";
+        textarea.style.height = (textarea.scrollHeight) + "px";
+    })
 }
+
+autoTextareaResizing();
--- chat/static_dev/js/hamburger_menu.js
+++ chat/static_dev/js/hamburger_menu.js
@@ -1,25 +1,30 @@
-let x = document.getElementById("myLinks");
-x.style.height = `${ x.scrollHeight }px`;
-window.getComputedStyle(x, null).getPropertyValue("height");
-x.style.height = "0px";
+const Menu = () => {
+  let x = document.getElementById("myLinks");
+  x.style.height = `${ x.scrollHeight }px`;
+  window.getComputedStyle(x, null).getPropertyValue("height");
+  x.style.height = "0px";
 
 
-setTimeout(function() {
-  x.style.display = "block";
-}, 500);
+  setTimeout(function () {
+    x.style.display = "block";
+  }, 500);
 
-x.addEventListener("transitionend", () => {
-  if (x.style.height != "0px") {
+  x.addEventListener("transitionend", () => {
+    if (x.style.height != "0px") {
       x.style.height = "auto"
-  }
-});
+    }
+  });
 
-function myFunction() {
-  if (x.style.height == "0px") {
-    x.style.height = `${ x.scrollHeight }px`
-  } else {
-    x.style.height = `${ x.scrollHeight }px`;
-    window.getComputedStyle(x, null).getPropertyValue("height");
-    x.style.height = "0px";
-  }
+  document.getElementById('js-menu').addEventListener('click', () => {
+    console.log(123)
+    if (x.style.height == "0px") {
+      x.style.height = `${ x.scrollHeight }px`
+    } else {
+      x.style.height = `${ x.scrollHeight }px`;
+      window.getComputedStyle(x, null).getPropertyValue("height");
+      x.style.height = "0px";
+    }
+  });
 }
+
+Menu();
