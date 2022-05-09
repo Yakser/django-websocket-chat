6dd6dd081cdb214b74aa3e005b19b78e9cb53e5e
Goddo-ro <r0manovic1@yandex.ru>
2022-05-10 00:16:56 +0300

    Восстановление позиции скрола чатов после обновления страницы

---
+++ chat/static_dev/js/restore_scroll_pos.js
@@ -0,0 +1,10 @@
+const chatsList = document.querySelector('.chats-list');
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    const scrollpos = localStorage.getItem('chatsScrollpos');
+    if (scrollpos) chatsList.scrollTo(0, scrollpos);
+});
+
+window.onbeforeunload = function(e) {
+    localStorage.setItem('chatsScrollpos', chatsList.scrollTop);
+}
