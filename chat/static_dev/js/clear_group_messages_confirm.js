8e9b946e4ee2d46011d4b7605b27b719a785487b
Goddo-ro <r0manovic1@yandex.ru>
2022-05-09 23:33:46 +0300

    Закрытия окна подтверждения без обновления страницы

--- chat/static_dev/js/clear_group_messages_confirm.js
+++ chat/static_dev/js/clear_group_messages_confirm.js
@@ -1,3 +1,11 @@
+const confirmation = document.querySelector('.confirmation');
+
+
 document.querySelector('.chat__clear-messages').addEventListener("click", function() {
-    document.querySelector('.confirmation').style.display = 'block';
+    confirmation.style.display = 'block';
+});
+
+
+document.querySelector('.close-btn').addEventListener("click", function() {
+    confirmation.style.display = 'none';
 });
