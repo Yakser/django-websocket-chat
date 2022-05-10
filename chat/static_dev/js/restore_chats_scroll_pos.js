77e9a9c7f96fbbd6c7d302a9698e76d36c808113
Goddo-ro <r0manovic1@yandex.ru>
2022-05-10 23:35:53 +0300

    Восстановление позиции скролла у сообщений после перезагрузки страницы

--- chat/static_dev/js/restore_chats_scroll_pos.js
+++ chat/static_dev/js/restore_chats_scroll_pos.js
@@ -1,14 +1,14 @@
-const restoreScrollPos = () => {
+const restoreChatsScrollPos = () => {
     const chatsList = document.querySelector('.chats-list');
 
     document.addEventListener("DOMContentLoaded", function (event) {
-        const scrollpos = localStorage.getItem('chatsScrollpos');
+        let scrollpos = localStorage.getItem('chatsScrollpos');
         if (scrollpos) chatsList.scrollTo(0, scrollpos);
     });
 
     window.onbeforeunload = function (e) {
-        localStorage.setItem('chatsScrollpos', chatsList.scrollTop);
+        if (chatsList) localStorage.setItem('chatsScrollpos', chatsList.scrollTop);
     }
 }
 
-restoreScrollPos();
+restoreChatsScrollPos();
