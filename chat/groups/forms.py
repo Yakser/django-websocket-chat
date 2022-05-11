c4e7eb92d6bdfde15cc5e782aa19dd94e83bd620
Sergey Yaksanov <iaksanov2044@mail.ru>
2022-05-11 19:34:59 +0400

    Форма редактирования группы

--- chat/groups/forms.py
+++ chat/groups/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 
+from groups.models import Group
+
 User = get_user_model()
 
 
@@ -21,6 +23,30 @@ class CreateGroupForm(forms.Form):
                            label='Имя группы',
                            help_text='Максимум 100 символов',
                            required=True)
+
+    group_members = forms.ModelMultipleChoiceField(
+        label='Участники',
+        queryset=User.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False)
+
+
+class EditGroupForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super(EditGroupForm, self).__init__(*args, **kwargs)
+
+        self.fields['group_members'].queryset = \
+            User.objects.filter(~Q(id=self.user.id)).only('id', 'username')
+
+    name = forms.CharField(max_length=100,
+                           label='Имя группы',
+                           help_text='Максимум 100 символов',
+                           required=True)
+
+    image = forms.ImageField(label='Аватар группы',
+                             required=False,
+                             allow_empty_file=False)
 
     group_members = forms.ModelMultipleChoiceField(
         label='Участники',
