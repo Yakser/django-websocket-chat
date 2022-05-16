9414c8f88af0ca269d6f53c44da4b0278cfe7027
Goddo-ro <r0manovic1@yandex.ru>
2022-05-16 21:39:29 +0300

    Настроил восстановление сайдбаров

--- chat/static_dev/js/page_reload_listener.js
+++ chat/static_dev/js/page_reload_listener.js
@@ -1,9 +1,13 @@
-const restoreMessagesScrollPos = () => {
+const pageReloadListener = () => {
     const chat = document.getElementById('chat-log');
+    const chatsList = document.querySelector('.chats-list');
 
     document.addEventListener("DOMContentLoaded", function (event) {
-        const scrollpos = localStorage.getItem('chatScrollpos');
+        let scrollpos = localStorage.getItem('chatScrollpos');
         if (scrollpos) chat.scrollTo(0, scrollpos);
+
+        scrollpos = localStorage.getItem('chatsScrollpos');
+        if (scrollpos) chatsList.scrollTo(0, scrollpos);
     });
 
     window.onbeforeunload = function (e) {
@@ -14,8 +18,8 @@ const restoreMessagesScrollPos = () => {
             }
             localStorage.setItem('chatScrollpos', pos);
         }
-        
+        if (chatsList) localStorage.setItem('chatsScrollpos', chatsList.scrollTop);
     }
 }
 
-restoreMessagesScrollPos();
+pageReloadListener();
