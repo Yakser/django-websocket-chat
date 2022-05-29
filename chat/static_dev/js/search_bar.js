2bc36219a2df12eaa3b36fb60d2360116565017a
Sergey Yaksanov <iaksanov2044@mail.ru>
2022-05-29 13:42:00 +0400

    Пофиксил поиски групп, пользователей и чатов

--- chat/static_dev/js/search_bar.js
+++ chat/static_dev/js/search_bar.js
@@ -1,28 +1,26 @@
 const clearString = (value) => {
     return value.trim().toLowerCase();
 }
-const listSearch = () => {
+const barSearch = () => {
     const input = document.getElementById('js-search');
-    const listContainer = document.getElementById('js-checkboxes-list-container').firstElementChild
+    const listContainer = document.getElementById('js-list-container')
     const elements = listContainer.children;
-
     input.addEventListener('input', () => {
         const clearedValue = clearString(input.value);
-
         if (clearedValue.length > 0) {
             for (let element of elements) {
                 if (clearString(element.textContent).includes(clearedValue)) {
-                    element.style.display = 'block';
+                    element.style.display = 'flex';
                 } else {
                     element.style.display = 'none';
                 }
             }
         } else {
             for (let element of elements) {
-                element.style.display = 'block';
+                element.style.display = 'flex';
             }
         }
     })
 }
 
-listSearch();
+barSearch();
